930f3b0a53b3888a82aa2b9f4e9df5638a4c8f2f
MacMat <matteo.machella01@gmail.com>
2024-01-20 10:50:59 +0100

    Implemented Curator object

---
+++ api/src/main/java/it/cs/unicam/MunicipalDigitalization/model/Curator.java
@@ -0,0 +1,35 @@
+package it.cs.unicam.MunicipalDigitalization.model;
+
+import it.cs.unicam.MunicipalDigitalization.io.ICurator;
+
+/**
+ * This class represents a curator, which is a type of authorized contributor.
+ * It extends the AuthorizedContributor class.
+ * A curator has a view and can validate points of interest (POIs).
+ */
+public class Curator extends AuthorizedContributor {
+
+    /**
+     * The view of the curator.
+     */
+    private ICurator view;
+
+    /**
+     * Constructor for the Curator class.
+     *
+     * @param name     The name of the curator.
+     * @param password The password of the curator.
+     * @param platform The platform of the curator.
+     */
+    public Curator(String name, String password, Platform platform) {
+        super(name, password, platform);
+    }
+
+    /**
+     * This method is used to validate a point of interest (POI).
+     * It delegates the validation to the view.
+     */
+    public void validatePOI() {
+        view.startValidatePOI();
+    }
+}
