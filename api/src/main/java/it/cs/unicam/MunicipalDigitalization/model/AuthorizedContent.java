916e56d262f23120644ba0575cc39bdcc413d221
Cristian Marinozzi <cristian.marinozzi1@gmail.com>
2024-01-25 00:39:18 +0100

    sets implemented

--- api/src/main/java/it/cs/unicam/MunicipalDigitalization/model/AuthorizedContent.java
+++ api/src/main/java/it/cs/unicam/MunicipalDigitalization/model/AuthorizedContent.java
@@ -4,7 +4,27 @@ import it.cs.unicam.MunicipalDigitalization.util.ContentType;
 import it.cs.unicam.MunicipalDigitalization.util.ID;
 
 public class AuthorizedContent extends AbstractContent {
-    public AuthorizedContent(ID id, ContentType type, IMunicipalElement municipalElement) {
-        super(id, type, municipalElement);
+    public AuthorizedContent(AuthenticatedUser author, IMunicipalElements municipalElement) {
+        super(author, municipalElement);
+    }
+
+    @Override
+    public void setType(ContentType type) {
+
+    }
+
+    @Override
+    public void setDescription(String description) {
+
+    }
+
+    @Override
+    public void setLink(String link) {
+
+    }
+
+    @Override
+    public void setPhoto(String photo) {
+
     }
 }
