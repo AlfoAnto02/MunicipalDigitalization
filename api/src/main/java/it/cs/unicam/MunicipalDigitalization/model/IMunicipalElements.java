67d529a81629c3b8cfbbfe596785e6ca746349a8
AlfoAnto02 <al.antognozzicaraffa@studenti.unicam.it>
2024-01-24 17:48:35 +0100

    Creation and abstraction of the MunicipalElements

---
+++ api/src/main/java/it/cs/unicam/MunicipalDigitalization/model/IMunicipalElements.java
@@ -0,0 +1,71 @@
+package it.cs.unicam.MunicipalDigitalization.model;
+
+import it.cs.unicam.MunicipalDigitalization.util.Coordinates;
+import it.cs.unicam.MunicipalDigitalization.util.ID;
+import java.util.Date;
+
+/**
+ * This interface represents the contract of a Municipal Elements.
+ * A class needs to implement this interface to be a Municipal Element such as
+ * a POI or an Itinerary.
+ *
+ */
+public interface IMunicipalElements {
+
+    /**
+     * Getter for the creation date of the Municipal Element.
+     *
+     * @return The creation date of the Municipal Element.
+     */
+
+    Date getCreationDate();
+
+    /**
+     * Getter for the author of the Municipal Element.
+     *
+     * @return The author of the Municipal Element.
+     */
+
+    AuthenticatedUser getUser();
+
+    /**
+     * This method is used to get the coordinates of the Municipal Element.
+     *
+     * @return The coordinates of the Municipal Element.
+     */
+
+    Coordinates getCoordinates();
+
+    /**
+     * This method is used to set the coordinates of the Municipal Element.
+     * The implementation should handle the setting of the coordinates.
+     *
+     * @param cc The new coordinates of the Municipal Element.
+     */
+
+    void setCoordinates(Coordinates cc);
+
+    /**
+     * This method is used to get the id of the Municipal Element.
+     *
+     * @return The id of the Municipal Element.
+     */
+
+    ID getId();
+
+    /**
+     * This method is used to get the name of the Municipal Element.
+     *
+     * @return The name of the Municipal Element.
+     */
+
+    String getName();
+
+    /**
+     * This method is used to set the name of the Municipal Element.
+     *
+     * @param name The new name of the Municipal Element.
+     */
+
+    void setName(String name);
+}
---
+++ api/src/main/java/it/cs/unicam/MunicipalDigitalization/model/MunicipalElements.java
@@ -0,0 +1,137 @@
+package it.cs.unicam.MunicipalDigitalization.model;
+
+import it.cs.unicam.MunicipalDigitalization.util.Coordinates;
+import it.cs.unicam.MunicipalDigitalization.util.ID;
+
+import java.time.Instant;
+import java.util.Date;
+
+/**
+ * This class represents a Municipal Element. A municipal Element is composed
+ * by an ID, an AutneticatedUser that create the Element, a creation Date of the Element
+ * the coordinates of the Element and the name of the Element.
+ *
+ */
+
+public abstract class MunicipalElements implements IMunicipalElements {
+
+    /**
+     * The unique identifier of the MunicipalElement.
+     */
+
+    private final ID id;
+
+    /**
+     * The author of the point of the MunicipalElement
+     */
+
+    private final AuthenticatedUser author;
+
+    /**
+     * The date when the point MunicipalElement was created
+     */
+
+    private final Date creationDate;
+
+    /**
+     * The coordinates of the MunicipalElement
+     */
+
+    private Coordinates coordinates;
+
+    /**
+     * The name of the MunicipalElement
+     */
+
+    private String name;
+
+    /**
+     * The constructor of the class
+     * @param user the Authenticated User that create this Element
+     */
+
+    public MunicipalElements(AuthenticatedUser user){
+        this.author=user;
+        this.id = new ID();
+        this.creationDate = Date.from(Instant.now());
+    }
+
+    /**
+     * This method is used to get the coordinates of the MunicipalElement
+     *
+     * @return The coordinates of the MunicipalElement
+     */
+
+    public Coordinates getCoordinates() {
+        return this.coordinates;
+    }
+
+    /**
+     * This method is used to set the coordinates of the MunicipalElement
+     * The implementation should handle the setting of the coordinates.
+     *
+     * @param coordinates The new coordinates of the MunicipalElement
+     */
+
+    public void setCoordinates(Coordinates coordinates) {
+        if (this.coordinates == null) {
+            throw new NullPointerException("Coordinates cannot be null");
+        }
+        this.coordinates.setX(coordinates.getX());
+        this.coordinates.setY(coordinates.getY());
+    }
+
+    /**
+     * This method is used to get the id of the MunicipalElement
+     *
+     * @return The id of the MunicipalElement
+     */
+
+    public ID getId() {
+        return this.id;
+    }
+
+    /**
+     * Setter for the name of the MunicipalElement
+     *
+     * @param name The new name of the MunicipalElement
+     */
+
+    public void setName(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new NullPointerException("Name cannot be null");
+        }
+        this.name = name;
+    }
+
+    /**
+     * This method is used to get the name of the MunicipalElement
+     *
+     * @return The name of the MunicipalElement
+     */
+
+    public String getName(){
+        return this.name;
+    }
+
+    /**
+     * Getter for the author of the MunicipalElement.
+     *
+     * @return The author of the MunicipalElement
+     */
+
+    public AuthenticatedUser getUser(){
+        return this.author;
+    }
+
+    /**
+     * Getter for the creation date of the MunicipalElement
+     *
+     * @return The creation date of the MunicipalElement
+     */
+
+    public Date getCreationDate() {
+        return this.creationDate;
+    }
+
+}
