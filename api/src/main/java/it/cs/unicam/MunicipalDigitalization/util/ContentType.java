b8633674fae176c96a6efa60a90ac1c4783ed8ee
MacMat <matteo.machella01@gmail.com>
2024-01-24 16:15:12 +0100

    ContentType enumeration implemented

--- api/src/main/java/it/cs/unicam/MunicipalDigitalization/util/ContentType.java
+++ api/src/main/java/it/cs/unicam/MunicipalDigitalization/util/ContentType.java
@@ -1,5 +1,32 @@
 package it.cs.unicam.MunicipalDigitalization.util;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public enum ContentType {
-    DESCRIPTION, LINK, IMAGE, VIDEO, AUDIO, FILE, LOCATION, DATE, TIME, DATETIME, NUMBER, TEXT, BOOLEAN, EMAIL, PHONE, URL, ADDRESS, PERSON, ORGANIZATION, EVENT, PRODUCT, SERVICE, OTHER
+    DESCRIPTION("Description"), LINK("Link"), IMAGE("Image");
+
+    private final String code;
+
+    ContentType(String code) {
+        this.code = code;
+    }
+
+    public static Optional<ContentType> selectType(String line) {
+        return Stream.of(ContentType.values()).filter(c -> c.isTypeOfLine(line)).findFirst();
+    }
+
+    public static List<Object> getListOfTypes() {
+        return Stream.of(ContentType.values()).map(ContentType::getCode).collect(Collectors.toList());
+    }
+
+    private Object getCode() {
+        return code;
+    }
+
+    private boolean isTypeOfLine(String line) {
+        return line.startsWith(code);
+    }
 }
