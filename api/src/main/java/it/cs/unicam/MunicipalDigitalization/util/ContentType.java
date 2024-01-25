5670bb528cd6837c35d8af09b440f40e335ab932
AlfoAnto02 <al.antognozzicaraffa@studenti.unicam.it>
2024-01-25 01:35:53 +0100

    Adjustment to ContentType (need to be fixed)

--- api/src/main/java/it/cs/unicam/MunicipalDigitalization/util/ContentType.java
+++ api/src/main/java/it/cs/unicam/MunicipalDigitalization/util/ContentType.java
@@ -1,14 +1,24 @@
 package it.cs.unicam.MunicipalDigitalization.util;
 
+import java.awt.*;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public enum ContentType {
-    DESCRIPTION("Description"), LINK("Link"), IMAGE("Image");
+    DESCRIPTION("Description"),
+    LINK("Link"),
+    IMAGE("Image");
 
     private final String code;
+
+    private String description;
+
+    private String link;
+
+    private Image image;
+
 
     ContentType(String code) {
         this.code = code;
@@ -29,4 +39,70 @@ public enum ContentType {
     private boolean isTypeOfLine(String line) {
         return line.startsWith(code);
     }
+
+    public void loadData(Object data) {
+        switch(this){
+            case DESCRIPTION:
+                loadDescription(data);
+                break;
+            case LINK:
+                loadLink(data);
+                break;
+            case IMAGE:
+                loadImage(data);
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value");
+        }
+    }
+
+    private void loadDescription(Object description) {
+        if (this == DESCRIPTION) {
+            if (description instanceof String) {
+                this.description = (String) description;
+            } else {
+                throw new IllegalArgumentException("Invalid data type for DESCRIPTION");
+            }
+        }
+    }
+
+    private void loadLink(Object link) {
+        if (this == LINK) {
+            if (link instanceof String) {
+                this.link = (String) link;
+            } else {
+                throw new IllegalArgumentException("Invalid data type for LINK");
+            }
+        }
+    }
+
+    private void loadImage(Object image) {
+        if (this == IMAGE) {
+            if (image instanceof Image) {
+                this.image = (Image) image;
+            } else {
+                throw new IllegalArgumentException("Invalid data type for IMAGE");
+            }
+        }
+    }
+
+    public Object returnContentType(){
+        if (!this.description.isEmpty()) return this.getDescription();
+        else if(!this.link.isEmpty()) return this.getLink();
+        return this.getImage();
+    }
+
+    private String getDescription() {
+        return description;
+    }
+
+    private String getLink() {
+        return link;
+    }
+
+    private Image getImage() {
+        return image;
+    }
+
+
 }
