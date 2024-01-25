bd740113a78429052592f6951b5ff6498c4f99f3
MacMat <matteo.machella01@gmail.com>
2024-01-25 11:01:27 +0100

    Code reformatted

--- api/src/main/java/it/cs/unicam/MunicipalDigitalization/io/IAuthorizedContributor.java
+++ api/src/main/java/it/cs/unicam/MunicipalDigitalization/io/IAuthorizedContributor.java
@@ -9,51 +9,43 @@ import it.cs.unicam.MunicipalDigitalization.util.ItineraryController;
 import it.cs.unicam.MunicipalDigitalization.util.POIController;
 
 /**
- *
  * This class represents an authorized contributor's view.
  * It implements the IContributorsView interface.
  * An authorized contributor can create points of interest (POIs) and itineraries.
  * It also has methods to set POI coordinates, show a list of types, set POI name, set type, show POI list, select POI, confirm itinerary, and set itinerary name.
- *
  */
 public class IAuthorizedContributor extends AbstractIContributorsView {
 
     /**
      * The POI controller of the authorized contributor.
-     *
      */
     private final POIController poiController;
 
     /**
      * The itinerary controller of the authorized contributor.
-     *
      */
     private final ItineraryController itineraryController;
 
     /**
      * The content controller of the authorized contributor.
-     *
      */
     private final ContentController contentController;
 
     /**
      * The municipality of the authorized contributor.
-     *
      */
     private final Municipality municipality;
 
     /**
      * The authorized contributor.
-     *
      */
     private final AuthorizedContributor contributor;
 
     /**
-     *
      * Constructor for the IAuthorizedContributor class.
      *
-     * @param municipality    The municipality of the authorized contributor.
-     * @param contributor The authorized contributor.
+     * @param municipality The municipality of the authorized contributor.
+     * @param contributor  The authorized contributor.
      */
     public IAuthorizedContributor(Municipality municipality, AuthorizedContributor contributor) {
         super(municipality);
