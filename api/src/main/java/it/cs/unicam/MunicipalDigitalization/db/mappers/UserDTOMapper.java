e49396230623f14be24095313e35f16759c8767b
AlfoAnto02 <al.antognozzicaraffa@studenti.unicam.it>
2024-02-23 00:35:22 +0100

    UserDTOMapper

---
+++ api/src/main/java/it/cs/unicam/MunicipalDigitalization/db/mappers/UserDTOMapper.java
@@ -0,0 +1,31 @@
+package it.cs.unicam.MunicipalDigitalization.db.mappers;
+
+import it.cs.unicam.MunicipalDigitalization.api.model.actors.AbstractAuthenticatedUser;
+import it.cs.unicam.MunicipalDigitalization.api.util.UserRole;
+import it.cs.unicam.MunicipalDigitalization.db.controllers.dto.UserDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
+
+/**
+ * This class is used to map the UserDTO to the User entity and vice versa.
+ */
+@Service
+public class UserDTOMapper implements Function<AbstractAuthenticatedUser, UserDTO>{
+
+    @Override
+    public UserDTO apply(AbstractAuthenticatedUser abstractAuthenticatedUser) {
+        if(abstractAuthenticatedUser.getRole().contains(UserRole.PLATFORM_GESTOR)){
+            return new UserDTO(
+                    abstractAuthenticatedUser.getName(),
+                    abstractAuthenticatedUser.getPassword(),
+                    null
+            );
+        }
+        return new UserDTO(
+                abstractAuthenticatedUser.getName(),
+                abstractAuthenticatedUser.getPassword(),
+                abstractAuthenticatedUser.getMunicipality().getId()
+        );
+    }
+}
